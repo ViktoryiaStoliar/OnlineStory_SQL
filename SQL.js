bb5ef1d4eccb9c51e3e137ab29811b2bb8b30e5f
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-10-23 23:19:43 +0300

    SQL

---
+++ SQL.js
@@ -0,0 +1,10 @@
+{
+//     create database onlinestory;
+//     use onlinestory;
+//     create table категория_товаров {
+// 	        id INT PRIMARY KEY AUTO_INCREMENT,
+//          product_category VARCHAR(40) NOT NULL
+//     };
+//     SELECT * FROM onlinestory.категория_товаров;
+//     INSERT INTO категория_товаров(product_category) VALUES('молочные продукты');
+}
