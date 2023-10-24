0b9230d4d2c01d7c07bf8aa40f09fdb21aa7067c
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-10-24 21:56:18 +0300

    SQL

--- SQL.js
+++ SQL.js
@@ -8,3 +8,48 @@
 //     SELECT * FROM onlinestory.категория_товаров;
 //     INSERT INTO категория_товаров(product_category) VALUES('молочные продукты');
 }
+
+// {use onlinestory;
+
+// create table products (
+// 	product_id INT PRIMARY KEY AUTO_INCREMENT,
+// 	категория_товаров_id int not null,
+//     product_name VARCHAR(20) NOT NULL,
+//     producer_producer_id int not null,
+//     price FLOAT NOT NULL,
+//     amount INT NOT NULL,
+    
+//     foreign key (категория_товаров_id) references категория_товаров (id) on delete cascade,
+//     foreign key (producer_producer_id) references producer (producer_id) on delete cascade
+// );
+
+// select * from категория_товаров;
+// select * from products;
+
+// create table producer (
+// 	producer_id INT PRIMARY KEY AUTO_INCREMENT,
+//     title VARCHAR(20) NOT NULL
+// );
+
+// create table cliens(
+// 	client_id INT PRIMARY KEY AUTO_INCREMENT,
+//     first_name varchar(30) not null,
+//     last_name varchar(30) not null,
+//     phone varchar(20) not null,
+//     address varchar(50) not null
+// );
+
+// create table payment(
+// 	payment_id INT PRIMARY KEY AUTO_INCREMENT,
+//     methodOfPayment varchar(30) not null
+// );
+ 
+// create table order (
+// 	категория_товаров_product_category VARCHAR(50) NOT NULL,
+//     products_product_name VARCHAR(20) NOT NULL,
+// 	products_price FLOAT NOT NULL,
+//     products_amount INT NOT NULL,
+//     deliveryTime datetime not null,
+//     payment_methodOfPayment varchar(30) not null,
+//     producer_title VARCHAR(20) NOT NULL
+// );}
